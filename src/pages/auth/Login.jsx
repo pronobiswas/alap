e96681f16abc7fcc12f4a300c1b54330b64e7b42
PRONOB BISWAS <biswaspronob@outlook.com>
2024-06-01 03:33:38 +0600

    working in login page validation

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -7,12 +7,17 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import { FcGoogle } from "react-icons/fc";
 import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 
 
 import LoginImages from '../../component/Utilities/loginImage.webp'
 import Images from '../../component/Utilities/Images';
 import { Link } from 'react-router-dom';
+import { Typography } from '@mui/material';
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -22,31 +27,109 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
 
 const Login = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const emailRegx = "^[A-Za-z0-9](([a-zA-Z0-9,=\.!\-#|\$%\^&\*\+/\?_`\{\}~]+)*)@(?:[0-9a-zA-Z-]+\.)+[a-zA-Z]{2,9}$"
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+
+    validationSchema: Yup.object({
+    
+      email: Yup.string()
+      .email('Invalid email address')
+      .matches(emailRegx , "regx check korchi")
+      .required('Required'),
+  
+      password: Yup.string()
+        .max(15, 'Must be 15 characters or less')
+        .min(5, "minum 5 charecter")
+        .required('Required')
+    }),
+
+    onSubmit: values => {
+      // alert(JSON.stringify(values, null, 2));
+      console.log(values);
+    }
+  })
+   
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container>
+
           <Grid item xs={6} className='center'>
             
               <div className="loginBox">
                 <h1 className='loginHeadding'>
-                Login to your account!
+                  Login to your account!
                 </h1>
                 <div className="logInWithGoogle">
                   <span className='logInWithGoogleIcon'><FcGoogle /></span>
                   <span className='logInWithGoogleTxt'>Login with Google</span>
                 </div>
-                <div className="inputTxt">
-                  <TextField id="outlined-basic" label="Email Address" variant="standard" className='inputItem'/>
-                </div>
-                <div className="inputTxt">
-                  <TextField id="outlined-basic" label="Password" variant="standard" className='inputItem'/>
-                </div>
-                <Button variant="contained" className='loginBtn'>LogIn to Continue</Button>
+
+                <form onSubmit={formik.handleSubmit}>
+                {/* //email TextField */}
+                  <div className="inputTxt">  
+                    <TextField 
+                    id="email" 
+                    label="Email Address" 
+                    variant="standard" 
+                    name="email"
+                    type="email"
+                    onChange={formik.handleChange} 
+                    value={formik.values.email} 
+                    className='inputItem'/>
+
+                    {formik.touched.email && formik.errors.email ? (
+                    <div>{formik.errors.email}</div>
+                    ) : null}
+
+                  </div>
+                  {/* password input fild */}
+                  <div className="inputTxt">
+                    <TextField 
+                    id="password" 
+                    name="password" 
+                    type="password"
+                    onChange={formik.handleChange} 
+                    value={formik.values.password}
+                    label="Password" 
+                    variant="standard" 
+                    className='inputItem'/>
+                    {formik.touched.password && formik.errors.password ? (
+                    <div>{formik.errors.password}</div>
+                    ) : null}
+                  </div>
+                  <Button variant="contained" className='loginBtn' type='submit'>LogIn to Continue</Button>
+
+                  <span onClick={handleOpen}>Forgot Password?</span>
+
+                </form>
+
                 <p>
-                  Don’t have an account ? <Link to="/reg">Sign Up</Link>
+                  Don’t have an account ! <Link to="/reg">Sign Up</Link>
                 </p>
               </div>
             
@@ -56,7 +139,39 @@ const Login = () => {
               <Images source={LoginImages} alt="logIn Images" styleing="loginImg"/>
             </div>
           </Grid>
+
         </Grid>
+
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2" style={{marginBottom:10}}>
+              Enter Your Email Address
+            </Typography>
+            <div className="inputTxt">  
+                    <TextField 
+                    id="email" 
+                    label="Email Address" 
+                    variant="standard" 
+                    name="email"
+                    type="email"
+                    onChange={formik.handleChange} 
+                    value={formik.values.email} 
+                    className='inputItem'/>
+
+                    {formik.touched.email && formik.errors.email ? (
+                    <div>{formik.errors.email}</div>
+                    ) : null}
+
+            </div>
+            <Button variant="contained" color="success">Reset Password</Button>
+          </Box>
+        </Modal>
+        
       </Box>
     </>
   )
