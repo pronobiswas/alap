ab31c15963629a46b7799ab6e78a3a2f71561d75
PRONOB BISWAS <biswaspronob@outlook.com>
2024-06-10 19:48:24 +0600

    login with google

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -99,16 +99,18 @@ const Login = () => {
   const handleGoogleSignin = ()=>{
     signInWithPopup(auth, provider)
     .then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
+      // console.log(user);
+      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
       const user = result.user;
-      navigate("/home") 
+      
+      // localStorage.setItem("loggedInUser" , JSON.stringify(user))
+      //   dispatch(logedinUser(user))
+        navigate("/home")
+      
       console.log(user);
     }).catch((error) => {
-      // const errorCode = error.code;
-      // const errorMessage = error.message;
-      // const email = error.customData.email;
-      // const credential = GoogleAuthProvider.credentialFromError(error);
+      
       console.log("user pai nai");
       // ...
     });
